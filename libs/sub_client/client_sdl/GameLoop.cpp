f08cc69a135131444c078392b8e8261751a10482
robyspr <robysprenger@gmail.com>
2022-11-28 22:17:13 -0300

    GameLoop updated

--- libs/sub_client/client_sdl/GameLoop.cpp
+++ libs/sub_client/client_sdl/GameLoop.cpp
@@ -3,12 +3,15 @@
 //
 
 #include <map>
-#include <utility>
 #include <unistd.h>
 #include "GameLoop.h"
+#include "sub_client/client_actions/ClientActionMove.h"
+#include "Ball.h"
+#include "Score.h"
+#include "sub_client/client_updates/ClientUpdateWorld.h"
 
-GameLoop::GameLoop(SDL2pp::Renderer &renderer, int xMax, int yMax, ProtectedQueue<ClientUpdate*> &updates,
-                   BlockingQueue<ClientAction*> &actions, Worldview &wv)
+GameLoop::GameLoop(SDL2pp::Renderer &renderer, int xMax, int yMax, ProtectedQueue<std::shared_ptr<ClientUpdate>> &updates,
+                   BlockingQueue<std::shared_ptr<ClientAction>> &actions, Worldview &wv)
         : renderer(renderer),
           updatesQueue(updates),
           actionsQueue(actions),
@@ -18,9 +21,12 @@ GameLoop::GameLoop(SDL2pp::Renderer &renderer, int xMax, int yMax, ProtectedQueu
           wv(wv){}
 
 void GameLoop::run() {
+    // Gameloop, notar como tenemos desacoplado el procesamiento de los inputs (handleEvents)
+    // del update del modelo.
     while (running) {
         handle_events();
         //pop from updates queue
+        popUpdates();
         update(FRAME_RATE);
         render();
         // la cantidad de segundos que debo dormir se debe ajustar en función
@@ -31,54 +37,29 @@ void GameLoop::run() {
 
 bool GameLoop::handle_events() {
     SDL_Event event;
+
     while(SDL_PollEvent(&event)){
         switch(event.type) {
             case SDL_KEYDOWN: {
                 // ¿Qué pasa si mantengo presionada la tecla?
-                SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
-                switch (keyEvent.keysym.sym) {
-                    case SDLK_LEFT:
-//                        player.moveLeft(this->xMax);
-                        break;
-                    case SDLK_RIGHT:
-//                        player.moveRight(this->xMax);
-                        break;
-                    case SDLK_UP:
-//                        player.moveUp(this->yMax);
-                        break;
-                    case SDLK_DOWN:
-//                        player.moveDown(this->yMax);
-                        break;
-                    case SDLK_ESCAPE:
-                        running = false;
-                        break;
+                auto &keyEvent = (SDL_KeyboardEvent&) event;
+                if(keyEvent.keysym.sym == SDLK_ESCAPE) {
+                    running = false;
+                    break;
                 }
-//                std::vector<uint8_t> movement(1);
-//                movement[0] = protocolo.getMapCommand(keyEvent.keysym.sym);
-//                Action action(MOVE,movement);
-//                actionsQueue.push(action);
+                uint8_t movement = directionMap.at(keyEvent.keysym.sym);
+                std::shared_ptr<ClientAction> action = std::make_shared<ClientActionMove>(movement,ON);
+                actionsQueue.push(action);
             } // Fin KEY_DOWN
                 break;
             case SDL_KEYUP: {
                 SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
-                switch (keyEvent.keysym.sym) {
-                    case SDLK_LEFT:
-//                        player.stopMovingX();
-                        break;
-                    case SDLK_RIGHT:
-//                        player.stopMovingX();
-                        break;
-                    case SDLK_UP:
-//                        player.stopMovingY();
-                        break;
-                    case SDLK_DOWN:
-//                        player.stopMovingY();
-                        break;
-                }
-                // poppear de la cola de updatesQueue
-                std::cout << "update to be popped of type: MOVE and data: " << std::endl;
-            }
-            case SDLK_ESCAPE:
+
+                uint8_t movement = directionMap.at(keyEvent.keysym.sym);
+                std::shared_ptr<ClientAction> action = std::make_shared<ClientActionMove>(movement,OFF);
+                actionsQueue.push(action);
+            }// Fin KEY_UP
+            case SDL_QUIT:
                 running = false;
                 break;
         } // fin switch(event)
@@ -94,6 +75,21 @@ void GameLoop::render() {
     renderer.Present();
 }
 
+void GameLoop::popUpdates() {
+    // TODO: implement swap between queues
+    std::shared_ptr<ClientUpdate> update;
+    while (!updatesQueue.tryPop(update)) {
+        Ball ball = update->getBall();
+        std::cout << "Ball: " << (int)ball.getX() << " " << (int)ball.getY() << std::endl;
+        Score score = update->getScore();
+        std::cout << "Score: " << (int)score.getLocal() << " " << (int)score.getVisitor() << std::endl;
+
+        std::vector<Car> players = update->getCars();
+        for (auto &player : players) {
+            std::cout << "Player: " << (int)player.getX() << " " << (int)player.getY() << std::endl;
+        }
+    }
+}
 void GameLoop::update(float dt) {
     wv.update(dt);
 }
